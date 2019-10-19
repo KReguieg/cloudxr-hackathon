f6a81cb7b89f236d50a714b9f918205fbf8c2e15
Timo Falcke <timo.falcke@gmail.com>
2019-10-19 11:56:22 +0200

    started working on movement

---
+++ Eye of the Skyger/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    Rigidbody rigid;
+
+    public float speed = 180;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("hello");
+        rigid = GetComponent<Rigidbody>();
+        rigid.velocity = new Vector3(0, 0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Eye of the Skyger/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public Obstacle obstaclePrefab;
+
+    public float spawnFrequency = 2f;
+
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if (timer >= spawnFrequency)
+        {
+            Vector3 spawnPos = Random.insideUnitCircle * 25;
+            Instantiate(obstaclePrefab, spawnPos + new Vector3(0, 0, -50), Quaternion.identity, transform);
+
+            timer -= spawnFrequency;
+        }
+    }
+}
