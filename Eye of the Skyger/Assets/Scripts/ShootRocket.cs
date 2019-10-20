1a96502aa93536c6d80cd5f0806626b0348348d8
DerHahn <christopher.hahn@exozet.com>
2019-10-20 12:33:56 +0200

    add

---
+++ Eye of the Skyger/Assets/Scripts/ShootRocket.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootRocket : ObjectToGaze
+{
+    float TriggerDistance = 2;
+    public override void GazeAt(Vector3 position)
+    {
+        if (Vector3.Distance(transform.position, position) <= TriggerDistance)
+            PlayerController.instance.Shoot(transform);
+    }
+}
