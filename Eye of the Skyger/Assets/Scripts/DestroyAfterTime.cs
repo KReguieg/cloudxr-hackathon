31976fb6b27a4279868e86789510aa38e43d34b2
Yän <yannick.pawils@web.de>
2019-10-20 08:54:25 +0200

    added EnemyDebris.prefab

---
+++ Eye of the Skyger/Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    public float timeToDestroy = 3;
+    float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeToDestroy)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
