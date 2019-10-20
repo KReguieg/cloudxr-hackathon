90bb485ae78d3f0b92f374759ae027b2802b7891
DerHahn <christopher.hahn@exozet.com>
2019-10-20 10:00:33 +0200

    added Intro/Menu

---
+++ Eye of the Skyger/Assets/Scripts/OnCollisionEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class OnCollisionEvent : MonoBehaviour
+{
+    [SerializeField] UnityEvent triggerdEvent;
+    private void OnCollisionEnter(Collision other) {
+        triggerdEvent.Invoke();
+    }
+}
