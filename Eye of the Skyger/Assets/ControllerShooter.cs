cbccb57de3286dd4761f16e9fb56774be676ffae
DerHahn <christopher.hahn@exozet.com>
2019-11-02 00:05:54 +0100

    limited shooting speed

--- Eye of the Skyger/Assets/ControllerShooter.cs
+++ Eye of the Skyger/Assets/ControllerShooter.cs
@@ -7,17 +7,22 @@ public class ControllerShooter : MonoBehaviour
     [SerializeField] LayerMask raycastLayer;
     [SerializeField] LineRenderer lineRenderer;
     [SerializeField] Transform ShootDirection;
-
+    [SerializeField] float ShootSpeed = 1;
+    float timer = 0;
     void Update()
     {
+        timer += Time.deltaTime;
         RaycastHit hit;
         lineRenderer.SetPosition(0, transform.position);
         if (Physics.Raycast(transform.position, ShootDirection.forward, out hit, 100, raycastLayer))
         {
             lineRenderer.SetPosition(1, hit.point);
             Enemy enemy = hit.collider.GetComponent<Enemy>();
-            if (enemy != null)
+            if (enemy != null && timer >= ShootSpeed)
+            {
                 enemy.TriggerShoot();
+                timer = 0;
+            }
         }
         else
         {
